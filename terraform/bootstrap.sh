449f06852d038388ab5384d0cc80d32bcd263fa8
theppasiva <theppasiva@gmail.com>
2024-12-05 21:01:14 +0530

    updated jenkinsfile and bootstrap.sh

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -13,7 +13,8 @@
 #!/bin/bash
 component=$1
 environment=$2 #dont use env here, it is reserved in linux
+app_version=$3
 yum install python3.12-devel python3.12-pip -y
 pip3.12 install ansible ansible-core==2.16 botocore boto3
-ansible-pull -U https://github.com/theppasiva/roboshop-ansible-roles-tf-1.git  -e component=$component -e env=$environment main-tf.yaml
+ansible-pull -U https://github.com/theppasiva/roboshop-ansible-roles-tf-1.git  -e component=$component -e env=$environment -e app_version=$app_version main-tf.yaml
 
