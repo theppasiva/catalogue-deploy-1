f3e6a71156681363d12eccb6007c2bfe56740855
theppasiva <theppasiva@gmail.com>
2025-04-14 21:52:38 +0530

    updated provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.94.1"
-      #version = "~> 5.0"
+      #version = "5.94.1"
+      version = "~> 5.0"
     }
   }
 
